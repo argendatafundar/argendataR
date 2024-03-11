5191837cb795073ca6435aa600b99be7b4765436
jgjuara <jg.juara@gmail.com>
2024-03-11 15:39:37 -0300

    agrego write_csv fundar flavour

---
+++ R/write_csv_fundar.R
@@ -0,0 +1,19 @@
+#' write_csv estilo Fundar
+#'
+#' @param x un dataframe a escribir como csv
+#' @param file nombre del archivo a escribir
+#'
+#' @return un archivo
+#' @export
+#'
+
+
+write_csv_fundar <- function(x, file) {
+    
+    x <- dplyr::mutate(x, dplyr::across(dplyr::everything(), as.character))
+     
+    readr::write_csv(x = x,
+                     file = file,
+                     quote = "all",
+                     na = "", eol = "\n")
+}

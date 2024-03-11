12e74a79ad1236dd112c17e90cbe33676a2e8859
jgjuara <jg.juara@gmail.com>
2024-03-11 12:07:35 -0300

    funciones de descarga agregan busqueda por nombre y param de directorio de descarga

--- R/descargar_fuente_clean.R
+++ R/descargar_fuente_clean.R
@@ -1,24 +1,49 @@
 #' Descarga fuente version clean
 #'
-#' @param id_fuente id numerico de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes()` 
-#'
-#' @return descarga la fuente seleccionada de Fuentes/raw
+#' @param id_fuente id numerico de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes_clean()` 
+#' @param nombre nombre de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes_clean()`
+#' @param dir directorio donde se descarga la fuente
+#' 
+#' @return file la fuente seleccionada de Fuentes/clean descargada en el directorio especificado
 #' @export
 #'
 
-descargar_fuente_clean <- function(id_fuente) {
+descargar_fuente_clean <- function(id_fuente, nombre, dir) {
+  
+  dir <- gsub("/$", "", dir)
+  
+  stopifnot("dir debe ser string de un directorio existente" = dir %in% gsub("^./","",list.dirs()))
+  
   
   df_fuentes <- fuentes_clean()
   
-  stopifnot("id fuente no es numeric o no fue encontrado en sheet `fuentes_clean()`" = is.numeric(id_fuente) & id_fuente %in% df_fuentes$id_fuente_clean)
   
-  fuente_path_clean <- df_fuentes[df_fuentes$id_fuente_clean == id_fuente,][["path_clean"]]
+  if (!missing(id_fuente)) {
+    stopifnot("id fuente no es numeric o no fue encontrado en sheet fuentes" = is.numeric(id_fuente) & id_fuente %in% df_fuentes$id_fuente)
+    
+    fuente_path_clean <- df_fuentes[df_fuentes$id_fuente_clean == id_fuente,][["path_clean"]]
+    
+    fuentes_clean_dir <- fuentes_clean_dir()$tree
+    
+    fuente_gd_id <- fuentes_clean_dir[fuentes_clean_dir$name == fuentes_clean_dir,][["id"]]
+    
+  } else if (!missing(nombre)) {
+    
+    stopifnot("'nombre' no es string o no fue encontrado en sheet `fuentes_clean()`" = is.character(nombre) & nombre %in% df_fuentes$nombre)
+    
+    fuente_path_raw <- df_fuentes[df_fuentes$nombre == nombre,][[ "path_raw"]]
+    
+    fuentes_raw_dir <- fuentes_raw_dir()$tree
+    
+    fuente_gd_id <- fuentes_raw_dir[fuentes_raw_dir$name == fuente_path_raw,][["id"]]
+    
+  } else {
+    stop("debe ingresar id_fuente o nombre")
+  }
   
-  fuentes_clean_dir <- fuentes_clean_dir()$tree
-  
-  fuente_gd_id <- fuentes_clean_dir[fuentes_clean_dir$name == fuentes_clean_dir,][["id"]]
   
   googledrive::drive_download(file = fuente_gd_id,
+                              path = glue::glue("{dir}/{fuente_path_raw}"),
                               overwrite = T)
   
 }
--- R/descargar_fuente_raw.R
+++ R/descargar_fuente_raw.R
@@ -1,24 +1,48 @@
 #' Descarga fuente version raw
 #'
-#' @param id_fuente id numerico de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes()` 
-#'
-#' @return descarga la fuente seleccionada de Fuentes/raw
+#' @param id_fuente id numerico de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes_raw()` 
+#' @param nombre nombre de la fuente tal cual aparece en la sheet de fuentes. Ver `fuentes_raw()`
+#' @param dir directorio donde se descarga la fuente
+#' 
+#' @return file la fuente seleccionada de Fuentes/raw descargada en el directorio especificado
 #' @export
 #'
 
-descargar_fuente_raw <- function(id_fuente) {
+descargar_fuente_raw <- function(id_fuente, nombre, dir) {
+  
+  dir <- gsub("/$", "", dir)
+  
+  stopifnot("dir debe ser string de un directorio existente" = dir %in% gsub("^./","",list.dirs()))
   
   df_fuentes <- fuentes_raw()
   
-  stopifnot("id fuente no es numeric o no fue encontrado en sheet fuentes" = is.numeric(id_fuente) & id_fuente %in% df_fuentes$id_fuente)
+  if (!missing(id_fuente)) {
+    stopifnot("id fuente no es numeric o no fue encontrado en sheet fuentes" = is.numeric(id_fuente) & id_fuente %in% df_fuentes$id_fuente)
+    
+    fuente_path_raw <- df_fuentes[df_fuentes$id_fuente == id_fuente,][[ "path_raw"]]
+    
+    fuentes_raw_dir <- fuentes_raw_dir()$tree
+    
+    fuente_gd_id <- fuentes_raw_dir[fuentes_raw_dir$name == fuente_path_raw,][["id"]]
+    
+  } else if (!missing(nombre)) {
+    
+    stopifnot("nombre fuente no fue encontrado en sheet fuentes" = nombre %in% df_fuentes$nombre)
+    
+    fuente_path_raw <- df_fuentes[df_fuentes$nombre == nombre,][[ "path_raw"]]
+    
+    fuentes_raw_dir <- fuentes_raw_dir()$tree
+    
+    fuente_gd_id <- fuentes_raw_dir[fuentes_raw_dir$name == fuente_path_raw,][["id"]]
+    
+  } else {
+    stop("debe ingresar id_fuente o nombre")
+  }
+  
 
-  fuente_path_raw <- df_fuentes[df_fuentes$id_fuente == id_fuente,][[ "path_raw"]]
-  
-  fuentes_raw_dir <- fuentes_raw_dir()$tree
-  
-  fuente_gd_id <- fuentes_raw_dir[fuentes_raw_dir$name == fuente_path_raw,][["id"]]
   
   googledrive::drive_download(file = fuente_gd_id,
+                              path = glue::glue("{dir}/{fuente_path_raw}"),
                               overwrite = T)
   
   }

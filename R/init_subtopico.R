b008b51c43a211f797186d58a750b0d82e9801ca
jgjuara <jg.juara@gmail.com>
2024-04-10 11:19:19 -0300

    funcion de iniciar subtopico y metadata2

--- R/init_subtopico.R
+++ R/init_subtopico.R
@@ -1,112 +1,150 @@
 #' Crear sripts y md del subtopico
 #'
 #' @param subtopico string codigo de 6 letras del subtopico
+#' @param scripts logical Si es TRUE (default) crea los esqueletos de scripts
+#' @param md logical Si es TRUE (default) crea un archivo md con la guia del subtopico
 #'
 #' @export
 #'
 
-init_subtopico <- function(subtopico) {
-  
-  meta <- metadata(subtopico)
-  
-  meta <- meta %>%
-    dplyr::distinct(dataset_archivo, script_archivo,
-                    fuente_nombre,
-                    url_path) %>%
+init_subtopico <- function(subtopico, scripts = T, md = T) {
+
+  metadata_subtopico <- metadata(subtopico)
+
+  meta <- metadata_subtopico %>%
+    dplyr::distinct(.data$dataset_archivo, .data$script_archivo,
+                    .data$fuente_nombre,
+                    .data$url_path) %>%
     dplyr::filter(!dplyr::if_all(dplyr::everything(),
                                  .fns = is.na))
-  
+
   meta$script_nuevo <- gsub("\\.([^.]*)$", "", meta$dataset_archivo)
-  
-  purrr::walk(unique(meta$script_nuevo), function(x) {
-    script_subtopico(
-      path = sprintf("scripts/subtopicos/%s/%s", subtopico,  x),
-      .navigate = F
-    )
-    
-  })
-  
+
+
+  if (isTRUE(scripts)) {
+
+      escritos_r <- purrr::map(unique(meta$script_nuevo), function(x) {
+
+        tryCatch(
+          {
+            script_subtopico(
+            path = sprintf("scripts/subtopicos/%s/%s", subtopico,  x),
+            .navigate = F)
+
+            TRUE
+            },
+          error  = function(e) {
+            warning(sprintf("El archivo %s ya existe, no fue sobreescrito.", x))
+            FALSE
+            }
+        )
+
+
+     })
+  }
+
+
+
   path_md <- sprintf("scripts/subtopicos/CAMCLI/%s.md", subtopico)
-  
-  msg <- sprintf("%s ya existe", path_md)
-  
-  stopifnot(msg = !file.exists(path_md))
-  
-  file.create(path_md)
-  
-  cat(
-    sprintf("# %s\n\n## Lista de datasets\n\n", subtopico),
-    file = path_md,
-    sep = "\n",
-    append = T
-  )
-  
-  purrr::walk(unique(meta$script_nuevo),
-              function(x) {
-                cat(
-                  sprintf("- [ ] %s", x),
-                  file = path_md,
-                  sep = "\n",
-                  append = T
-                )
-              })
-  
-  cat(
-    sprintf("\n\n## Referencias datasets\n\n"),
-    file = path_md,
-    sep = "\n",
-    append = T
-  )
-  
-  purrr::walk(unique(meta$script_nuevo),
-              function(x) {
-                df_x <- meta %>%
-                  dplyr::filter(script_nuevo == x)
-                
-                cat(
-                  sprintf("\n- `%s`:\n", x),
-                  file = path_md,
-                  sep = "\n",
-                  append = T
-                )
-                
-                cat(
-                  sprintf("  **Scripts:**", x),
-                  file = path_md,
-                  sep = "\n",
-                  append = T
-                )
-                
-                purrr::walk(unique(df_x$script_archivo), function(x) {
-                  cat(
-                    sprintf("-- %s", x),
-                    file = path_md,
-                    sep = "\n",
-                    append = T
-                  )
-                  
-                })
-                cat(
-                  sprintf("  **Fuentes:**", x),
-                  file = path_md,
-                  sep = "\n",
-                  append = T
-                )
-                
-                df_y <- df_x %>%
-                  dplyr::distinct(fuente_nombre, url_path)
-                
-                purrr::walk2(df_y$fuente_nombre, df_y$url_path, function(x, y) {
-                  cat(
-                    sprintf("-- %s: %s", x, y),
-                    file = path_md,
-                    sep = "\n",
-                    append = T
-                  )
-                  
-                })
-                
-                
-              })
-  
+
+
+  if (isTRUE(md)) {
+
+
+    if (file.exists(path_md)) {
+      warning(sprintf("%s ya existe.No se sobreescribio el archivo", path_md))
+      escrito_md <- FALSE
+    } else {
+
+      file.create(path_md)
+
+
+
+      cat(
+        sprintf("# %s\n\n## Lista de datasets\n\n", subtopico),
+        file = path_md,
+        sep = "\n",
+        append = T
+      )
+
+      purrr::walk(unique(meta$script_nuevo),
+                  function(x) {
+                    cat(
+                      sprintf("- [ ] %s", x),
+                      file = path_md,
+                      sep = "\n",
+                      append = T
+                    )
+                  })
+
+      cat(
+        sprintf("\n\n## Referencias datasets\n\n"),
+        file = path_md,
+        sep = "\n",
+        append = T
+      )
+
+      purrr::walk(unique(meta$script_nuevo),
+                  function(x) {
+                    df_x <- meta %>%
+                      dplyr::filter(.data$script_nuevo == x)
+
+                    cat(
+                      sprintf("\n- `%s`:\n", x),
+                      file = path_md,
+                      sep = "\n",
+                      append = T
+                    )
+
+                    cat(
+                      "  **Scripts:**",
+                      file = path_md,
+                      sep = "\n",
+                      append = T
+                    )
+
+                    purrr::walk(unique(df_x$script_archivo), function(x) {
+                      cat(
+                        sprintf("-- %s", x),
+                        file = path_md,
+                        sep = "\n",
+                        append = T
+                      )
+
+                    })
+                    cat(
+                      "  **Fuentes:**",
+                      file = path_md,
+                      sep = "\n",
+                      append = T
+                    )
+
+                    df_y <- df_x %>%
+                      dplyr::distinct(.data$fuente_nombre, .data$url_path)
+
+                    purrr::walk2(df_y$fuente_nombre,
+                                 df_y$url_path, function(x, y) {
+                                   cat(
+                                     sprintf("-- %s: %s", x, y),
+                                     file = path_md,
+                                     sep = "\n",
+                                     append = T
+                                   )
+
+                                 })
+
+                  })
+
+
+      escrito_md <-  TRUE
+
+    }
+
+  }
+
+  escritos_r <- sum(unlist(escritos_r))
+  escrito_md <- sum(escrito_md)
+
+  message(sprintf("Se crearon %d archivos .R y %d archivo .md", escritos_r, escrito_md))
+
 }
--- R/metadata.R
+++ R/metadata.R
@@ -8,41 +8,42 @@
 
 
 metadata <- function(subtopico = NULL) {
-  
+
   stopifnot("'subtopico' debe ser string con codigo de 6 letras de subtopico" = is.character(subtopico))
-  
+
   subtopico <- toupper(subtopico)
-  
+
   # Lista los archivos o carpetas dentro de la carpeta de subtemas utilizando su ID
   paths_subtopicos <- subtopicos_dir()$tree
-  
+
   stopifnot("'subtopico' no hallado en el drive" = any(grepl(subtopico, paths_subtopicos$name)))
-  
+
   paths_subtopicos <- paths_subtopicos[grepl(subtopico, paths_subtopicos$name),]
-  
+
   # Para cada ID en paths_subtopicos, lista los archivos dentro y los recopila en una lista
   files_subtopicos <- purrr::map(paths_subtopicos$id, function(x) {
     googledrive::drive_ls(googledrive::as_id(x))
   })
-  
+
   # Combina los dataframes de archivos de subtemas en uno solo
   files_subtopicos <- dplyr::bind_rows(files_subtopicos)
-  
+
   # Filtra los archivos de metadatos que contienen "argendata -" en su nombre, excluyendo aquellos que contienen "ejemplo"
-  metadata_files <- files_subtopicos %>% 
-    dplyr::filter(grepl("argendata -", tolower(name))) %>% 
-    dplyr::filter(! grepl("ejemplo", tolower(name)))
-  
+  metadata_files <- files_subtopicos %>%
+    dplyr::filter(grepl("argendata -", tolower(.data$name))) %>%
+    dplyr::filter(! grepl("ejemplo", tolower(.data$name)))
+
   # Para cada archivo de metadatos, lee su contenido saltando las primeras 6 filas y asumiendo que las columnas son tipo texto
   metadata <- purrr::map2(metadata_files$id, metadata_files$name, function(x, y) {
-    googlesheets4::read_sheet(x, skip = 6, col_types = "c") %>% 
+    df <- googlesheets4::read_sheet(x, skip = 6, col_types = "c")
+    df %>%
       dplyr::mutate(subtopico_nombre = gsub("ArgenData - ", "", y))
   })
-  
+
   # Combina los datos leídos de todos los archivos de metadatos en un único dataframe
   metadata <- dplyr::bind_rows(metadata)
-  
+
   # Filtra el dataframe resultante para eliminar las filas donde todos los valores son NA (datos faltantes)
-  metadata <- metadata %>% 
+  metadata <- metadata %>%
     dplyr::filter(!dplyr::if_all(dplyr::everything(), is.na))
 }

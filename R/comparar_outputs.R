a4681c0626c52a0cae3d390effaa4ffd1987495a
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2024-05-02 17:51:43 -0300

    mejora de docu

--- R/comparar_outputs.R
+++ R/comparar_outputs.R
@@ -6,7 +6,13 @@
 #' @param entrega_subtopico string nombre de la carpeta de entrega donde buscar el output
 #' @param pk string Vector con los nombres de columnas que son primary key del dataframe
 #' @param drop_output_drive logical Si es FALSE (default) el resultado incluye el output cargado en el drive usado para comparar. Si es TRUE no lo incluye.
-#'
+#' @description
+#' El resultado es una lista que contiene cuales son las columnas faltantes (cols_faltantes), cuales son las columnas nuevas (cols_nuevas),
+#' un dataframe que compara las clases de columnas viejas y columnas nuevas (comparacion_clases), 
+#' la cantidad de filas de diferencia entre el nuevo dataset y el viejo (diferencia_nfilas), y una lista con metricas de comparacion por columna (comparacion_cols).
+#' La lista de comparacion_cols presenta por cada columna la cantidad de nuevos NA (nuevos_na), la cantidad de mismatches para la columna en funcion de los PK (suma_mismatches) y
+#' ademas un dataframe (metricas_filas) que indica para la columna analizada para cada tupla de valores PK las diferencias absolutas (abs(Xi/Xi-1)) y diferencias relativas (abs(Xi/Xi-1)/abs(Xi-1))
+#' 
 #' @return Devuelve una lista con los resultados de los chequeos realizados
 #' @export
 #'

e6476983ed68661e3230f43047f59f1ee69b36d6
jgjuara <jg.juara@gmail.com>
2024-05-15 10:44:06 -0300

    fix bug en manejo de codigos de actualizacion fuente clean

--- R/actualizar_fuente_clean.R
+++ R/actualizar_fuente_clean.R
@@ -7,17 +7,19 @@
 #' La fecha será actualizada usando `Sys.time()` al momento de su ejecución.
 #'
 #' @param id_fuente_clean integer id numerico que permite seleccionar la fuente segun aparece en el sheet. Para consultar ids usar  `fuentes_clean()`
-#' @param dir string Ruta al directorio desde el cual cargar el archivp
+#' @param directorio string Ruta al directorio desde el cual cargar el archivo. Si es NULL toma tempdir()
 #' @export
 #'
 #'
 
-actualizar_fuente_clean <- function(id_fuente_clean, dir = NULL) {
+actualizar_fuente_clean <- function(id_fuente_clean, directorio = NULL) {
+  
+  limpiar_temps()
 
-  if (is.null(dir)) {
-    dir <- tempdir()
+  if (is.null(directorio)) {
+    directorio <- tempdir()
   } else {
-    stopifnot("'dir' debe ser string a una ruta valida" = dir.exists(dir))
+    stopifnot("'directorio' debe ser string a una ruta valida" = dir.exists(directorio))
   }
 
   stopifnot("'id_fuente_clean' debe ser id numerico de fuente o character con codigo de fuente" = is.numeric(id_fuente_clean) | is.character(id_fuente_clean))
@@ -27,45 +29,44 @@ actualizar_fuente_clean <- function(id_fuente_clean, dir = NULL) {
   if (is.numeric(id_fuente_clean)) {
 
     stopifnot("'id_fuente_clean' no encontrado en sheet de fuentes. Ver `fuentes_clean()`." = id_fuente_clean %in% df_fuentes$id_fuente_clean )
+    
+    df_fuentes <- df_fuentes[df_fuentes$id_fuente_clean == id_fuente_clean,]
+    
 
   } else if (is.character(id_fuente_clean)) {
 
     stopifnot("'id_fuente_clean' no coincide con ningun codigo en sheet de fuentes. Ver `fuentes_clean()`." = id_fuente_clean %in% df_fuentes$codigo )
-    id_fuente_clean <- regmatches(id_fuente_clean, m = regexpr("(?<=R)(\\d+)", text = id_fuente_clean, perl = T))
 
-    id_fuente_clean <- as.numeric(id_fuente_clean)
+    df_fuentes <- df_fuentes[df_fuentes$codigo == id_fuente_clean,]
+    
   }
+    
 
 
 
-   fecha <- Sys.time()
-
-  inputs <- list(
-    "id_fuente_clean" = id_fuente_clean,
-    "fecha" = fecha
-    )
-
-  df_fuentes <- df_fuentes[df_fuentes$id_fuente_clean == inputs$id_fuente_clean,]
-
-  df_fuentes$fecha <-  inputs$fecha
+  fecha <- Sys.time()
 
 
-  if (!file.exists(normalizePath(paste(dir, df_fuentes$path_clean, sep = "/")))) {
+  df_fuentes$fecha <-  fecha
+
+
+  if (!file.exists(normalizePath(paste(directorio, df_fuentes$path_clean, sep = "/")))) {
     stop("No se encontro el archivo clean, guardarlo en la ubicacion antes de continuar")
   }
 
 
-  print( df_fuentes[df_fuentes$id_fuente_clean == inputs$id_fuente_clean ,])
+  print( df_fuentes)
 
-  googledrive::drive_upload(media = normalizePath(paste(dir, df_fuentes$path_clean, sep = "/")),
+  googledrive::drive_upload(media = normalizePath(glue::glue("{directorio}/{df_fuentes$path_clean}")),
                             path = googledrive::as_id(fuentes_clean_dir()$id),
-                            name = df_fuentes$path_clean, overwrite = T)
+                            name = df_fuentes$path_clean,
+                            overwrite = T)
 
 
   df_fuentes %>%
     googlesheets4::range_write(col_names = F,
                                ss = fuentes_clean_sheet_id(),
-                               range = sprintf("A%d:H%d", id_fuente_clean+1, id_fuente_clean+1))
+                               range = sprintf("A%d:H%d", df_fuentes$id_fuente_clean+1, df_fuentes$id_fuente_clean+1))
 
 
 

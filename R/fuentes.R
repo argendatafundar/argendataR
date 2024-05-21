524f0448cdfa71397921d342ba7fc05c00f79d11
jgjuara <jg.juara@gmail.com>
2024-05-21 20:52:39 -0300

    cambio default limpiar cache

--- R/fuentes.R
+++ R/fuentes.R
@@ -1,30 +1,30 @@
-#' Fuentes 
-#' 
+#' Fuentes
+#'
 #' @param limpiar_cache  Logical Si es TRUE borra el cache de consultas al drive de argendata. Si es FALSE reutiliza el cache existente.
 #' @return tibble con la sheet de fuentes raw+clean de googledrive Argendata
 #' @export
 #'
 
-fuentes <- function(limpiar_cache = F) {
-  
+fuentes <- function(limpiar_cache = T) {
+
   if(isTRUE(limpiar_cache)){
     limpiar_temps()
   }
-  
-  df_r <- fuentes_raw()
-  df_r <- df_r %>% 
+
+  df_r <- fuentes_raw(limpiar_cache = F)
+  df_r <- df_r %>%
     dplyr::rename_with(.fn = function(x) {paste0(x,"_raw")})
-  
-  df_c <- fuentes_clean()  
-  
-  df_c <- df_c %>% 
+
+  df_c <- fuentes_clean(limpiar_cache = F)
+
+  df_c <- df_c %>%
     dplyr::rename_with(.cols = -c("id_fuente_raw", "id_fuente_clean", "path_clean"),
                 .fn = function(x) paste0(x, "_clean"))
-  
-  df_c <- dplyr::left_join(df_c, df_r, by = c("id_fuente_raw" = "id_fuente_raw"))  
-  
+
+  df_c <- dplyr::left_join(df_c, df_r, by = c("id_fuente_raw" = "id_fuente_raw"))
+
 
   df <- dplyr::bind_rows(df_r, df_c)
-  
+
   df
 }

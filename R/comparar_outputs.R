db72b082872a61e65c6a34d7886815b7234ef67f
jgjuara <jg.juara@gmail.com>
2024-06-12 10:30:37 -0300

    comparar outputs admite un df anterior de referencia o parametros para descarga

--- R/comparar_outputs.R
+++ R/comparar_outputs.R
@@ -1,7 +1,10 @@
 #' Comparar un data.frame con un output cargado en el drive de Argendata como csv
 #'
 #' @param df data.frame dataframe a comparar con el output cargado en el drive
-#' @param df_anterior data.frame dataframe anterior que se usa como referencia de comparacion. Ver `argendataR::descargar_output()`
+#' @param df_anterior data.frame dataframe anterior que se usa como referencia de comparacion. Ver `argendataR::descargar_output()`.
+#' @param nombre  string nombre del output en el drive para realizar la descarga si no se proveyo `df_anterior`
+#' @param subtopico string subtopico al que pertenece el output para realizar la descarga si no se proveyo `df_anterior`. Si no esta definido el parametro busca en el ambiente global de la sesion un objeto llamado "subtopico"
+#' @param entrega_subtopico string nombre de la carpeta de entrega donde buscar el output para realizar la descarga si no se proveyo `df_anterior`
 #' @param pk string Vector con los nombres de columnas que son primary key del dataframe
 #' @param k_control_num numeric cantidad de desvios estandar a partir del cual se seleccionan posibles outliers
 #' @param drop_joined_df logical Si es FALSE (default) el resultado incluye joined_df: el left_join de el output cargado en el drive con el df. Si es TRUE no lo incluye.
@@ -16,8 +19,28 @@
 #' @export
 #'
 
-comparar_outputs <- function(df, df_anterior, pk = NULL, k_control_num = 3,
+comparar_outputs <- function(df, df_anterior,
+                             nombre, subtopico,
+                             entrega_subtopico = "primera_entrega",
+                             pk = NULL, k_control_num = 3,
                              drop_joined_df = F) {
+  
+  if (missing(df_anterior)) {
+    
+    message("No se recibio un dataframe de referencia. Se procede a descarga desde el drive")
+    
+    if (missing(subtopico)) {
+      
+      subtopico <- get("subtopico", envir = globalenv())
+    }
+    
+    stopifnot("'entrega_subtopico' debe ser uno de: primera_entrega, segunda_entrega, datasets_update" = entrega_subtopico %in% c("primera_entrega", "segunda_entrega", "datasets_update"))
+    
+    df_anterior <- descargar_output(nombre = nombre,
+                                    subtopico = subtopico,
+                                    entrega_subtopico = entrega_subtopico)
+    
+  }
 
 
   columns_check <- check_cols(df = df, df_anterior = df_anterior)

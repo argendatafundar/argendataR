bd672b41f9906097caef1732a40ace23482a86da
jgjuara <jg.juara@gmail.com>
2024-09-11 20:26:57 -0300

    borro param df

--- R/actualizar_fuente_raw.R
+++ R/actualizar_fuente_raw.R
@@ -30,7 +30,6 @@ actualizar_fuente_raw <- function(id_fuente,
                                   actualizable = NULL,
                                   fecha_descarga = NULL,
                                   fecha_actualizar = NULL,
-                                  df = NULL,
                                   path_raw = NULL,
                                   script = NULL,
                                   api = NULL,
@@ -48,19 +47,19 @@ actualizar_fuente_raw <- function(id_fuente,
   stopifnot("'id_fuente' debe ser id numerico de fuente o character con codigo de fuente" = is.numeric(id_fuente) | is.character(id_fuente))
 
   df_fuentes_raw <- fuentes_raw()
-  
+
   df_fuentes_raw_md5 <- tools::md5sum(glue::glue("{RUTA_FUENTES()}/fuentes_raw.csv"))
-  
+
 
   if (is.numeric(id_fuente)) {
 
     stopifnot("'id_fuente' no encontrado en sheet de fuentes. Ver `fuentes_raw()`." = id_fuente %in% df_fuentes_raw$id_fuente )
-    
+
     irow <- which(df_fuentes_raw$id_fuente == id_fuente)
-    
+
     stopifnot("Mas de una coincidencia de id_fuente hallada" = length(irow) == 1)
-    
-    
+
+
 
   } else if (is.character(id_fuente)) {
 
@@ -68,34 +67,34 @@ actualizar_fuente_raw <- function(id_fuente,
     id_fuente <- regmatches(id_fuente, m = regexpr("(?<=R)(\\d+)", text = id_fuente, perl = T))
 
     id_fuente <- as.numeric(id_fuente)
-    
+
     irow <- which(df_fuentes_raw$id_fuente == id_fuente)
-    
+
     stopifnot("Mas de una coincidencia de id_fuente hallada" = length(irow) == 1)
-    
-    
+
+
   }
 
-  
+
   if (is.null(fecha_descarga)) {
     fecha_descarga <- Sys.time()
-    
-  } 
 
-  
+  }
+
+
   if (is.character(fecha_actualizar) & fecha_actualizar != "Sin informacion") {
-    
+
     fecha_actualizar <- as.Date(fecha_actualizar)
     stopifnot("param 'fecha_actualizar' debe ser fecha valida o string parseable como fecha o 'Sin informacion'" = !is.na(fecha_actualizar) & length(fecha_actualizar) == 1)
-    
+
   } else if (class(fecha_actualizar) %in% c("Date", "POSIXct", "POSIXt")) {
-    
+
     stopifnot("param 'fecha_actualizar' debe ser fecha valida o string parseable como fecha o 'Sin informacion'" = !is.na(fecha_actualizar) & length(fecha_actualizar) == 1)
-    
+
   } else {
-    
+
     stopifnot("param 'fecha_actualizar' debe ser fecha o character parseable a fecha o 'Sin informacion'" = fecha_actualizar == "Sin informacion" & is.character(fecha_actualizar))
-    
+
   }
 
 
@@ -122,7 +121,7 @@ actualizar_fuente_raw <- function(id_fuente,
   inputs <- inputs[sapply(inputs, function(x) !is.null(x))]
 
 
-  
+
 
 
   if (!isFALSE(prompt) & length(inputs) > 1) {
@@ -143,41 +142,41 @@ actualizar_fuente_raw <- function(id_fuente,
     stop()
 
   }
-  
-  
+
+
   for (i in names(inputs)) {
-    
+
     inputs[[i]] <- coerce_to(inputs[[i]], df_fuentes_raw[[irow, i]])
-    
+
     df_fuentes_raw[[irow, i]] <-  inputs[[i]]
-    
+
   }
-  
+
   print(df_fuentes_raw[irow,])
-  
-    
+
+
   stopifnot("El registro de fuentes cambio antes de finalizar la actualizacion. Vuelva a intentarlo" = df_fuentes_raw_md5 == tools::md5sum(glue::glue("{RUTA_FUENTES()}/fuentes_raw.csv")))
-    
-  
+
+
   df_fuentes_raw %>%
     readr::write_csv(file = glue::glue("{RUTA_FUENTES()}/fuentes_raw.csv"), eol = "\n", progress = F)
-  
+
   message("Registro actualizado en fuentes raw")
-  
+
   if (file.exists(normalize_path(paste(directorio, inputs$path_raw, sep = "/")))) {
-    
-    
-    
+
+
+
     file.copy(from = glue::glue("{directorio}/{inputs$path_raw}"),
               to = glue::glue("{RUTA_FUENTES()}/raw/{inputs$path_raw}"), overwrite = T, copy.mode = T)
-    
+
     message("Fuente copiada a carpeta raw")
-    
-    
+
+
   } else {
     stop("Error inesperado al guardar el archivo")
   }
-  
+
 
 }
 

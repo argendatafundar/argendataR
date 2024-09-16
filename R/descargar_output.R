b6e7285740d6b43121989f2f3e5695bd34fbef62
jgjuara <jg.juara@gmail.com>
2024-09-16 18:14:58 -0300

    descargar de gh

--- R/descargar_output.R
+++ R/descargar_output.R
@@ -15,23 +15,49 @@
 descargar_output <- function(nombre, subtopico, entrega_subtopico, ...) {
     
     limpiar_temps()
+  
+    path <- gsub("\\.csv$","",nombre)
     
-    # dowload or read output
-    subtopico_outputs_df <- subtopico_outputs(subtopico_nombre = subtopico,
-                                              entrega_subtopico = entrega_subtopico)
+    path <- paste0(path, ".csv")
+  
+    output_url <- glue::glue("{GH_DATA_RAWURL()}/{subtopico}/{path}") 
     
-    id_output <- subtopico_outputs_df$id[grepl(nombre, subtopico_outputs_df$name)]
+    df <- tryCatch(
+      httr2::req_perform(httr2::request(output_url)),
+      httr2_http_404 = function(cnd) NULL
+    )
     
-    filetemp <- tempfile(pattern = sprintf("%s_%s_%s_argdt",
-                                          nombre,
-                                          entrega_subtopico,
-                                          subtopico),
-                         fileext = ".csv")
+    if (is.null(df)) {
+      warning("Output no encontrado en repo 'data'.")
+      # dowload or read output
+      subtopico_outputs_df <- subtopico_outputs(subtopico_nombre = subtopico,
+                                                entrega_subtopico = entrega_subtopico)
+      
+      id_output <- subtopico_outputs_df$id[grepl("cosasoa", subtopico_outputs_df$name)]
+      
+      stopifnot("Output no encontrada en el drive de Argendata" = length(id_output) != 0)
+      
+      stopifnot("Se encontro mas de una coincidencia en el drive de Argendata. Corregir filesystem del drive" = length(id_output) != 1)
+      
+      filetemp <- tempfile(pattern = sprintf("%s_%s_%s_argdt",
+                                             nombre,
+                                             entrega_subtopico,
+                                             subtopico),
+                           fileext = ".csv")
+      
+      googledrive::drive_download(file = googledrive::as_id(id_output),
+                                  path = filetemp)
+      
+      output_drive <-  readr::read_delim(filetemp, ...)
     
-    googledrive::drive_download(file = googledrive::as_id(id_output),
-                                path = filetemp)
+    } else {
+      
+      output_drive <- rvest::html_text(rvest::read_html(df$body)) %>%
+        readr::read_csv()
+      
+    }
     
-    output_drive <-  readr::read_delim(filetemp, ...)
+    
     
     
     output_drive

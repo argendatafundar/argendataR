07a3b9a77ee953a19cf738f3b681903125561d80
jgjuara <jg.juara@gmail.com>
2024-10-23 15:01:12 -0300

    force message

--- R/actualizar_fuente_raw.R
+++ R/actualizar_fuente_raw.R
@@ -212,13 +212,13 @@ actualizar_fuente_raw <- function(id_fuente,
 
   if (file.exists(normalize_path(paste(directorio, inputs$path_raw, sep = "/")))) {
 
-    print(glue::glue("FROM: {directorio}/{inputs$path_raw}"))
-    print(glue::glue("TO: {RUTA_FUENTES()}/raw/{inputs$path_raw}"))
+    message(glue::glue("FROM: {directorio}/{inputs$path_raw}"))
+    message(glue::glue("TO: {RUTA_FUENTES()}/raw/{inputs$path_raw}"))
 
     check_copy <- file.copy(from = glue::glue("{directorio}/{inputs$path_raw}"),
               to = glue::glue("{RUTA_FUENTES()}/raw/{inputs$path_raw}"), overwrite = T)
 
-    print(glue::glue("Check copy: {check_copy}"))
+    message(glue::glue("Check copy: {check_copy}"))
 
     if (isFALSE(check_copy)) {
 

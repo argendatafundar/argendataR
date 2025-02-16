1c6557e64be9c16e81f484db02a15116a5fccd12
jgjuara <jg.juara@gmail.com>
2025-02-16 16:07:23 -0300

    fix directorio remoto path

--- R/gh.R
+++ R/gh.R
@@ -1,6 +1,6 @@
 #' Subir un nuevo archivo a un repositorio GitHub
 #'
-#' @param path_remoto Ruta completa del archivo en el repositorio remoto.
+#' @param path_remoto Ruta completa del archivo en el repositorio remoto (puede incluir carpetas).
 #' @param path_local Ruta del archivo local.
 #' @param repo_owner Usuario o organización dueña del repositorio.
 #' @param repo_name Nombre del repositorio.
@@ -36,53 +36,90 @@ actualizar_archivo <- function(path_remoto, path_local, sha, repo_owner, repo_na
 
 #' Subir o actualizar un archivo en un repositorio GitHub
 #'
-#' @param nombre_archivo Nombre del archivo a subir o actualizar.
+#' @param path_local Ruta completa del archivo local. (Por ejemplo '/tmp/data.json')
+#' @param path_remoto Ruta en donde se subirá o actualizará el archivo en el repositorio remoto
+#'        (Por ejemplo 'CRECIM/data.json')
 #' @param repo_owner Usuario o organización dueña del repositorio.
 #' @param repo_name Nombre del repositorio.
 #' @param branch Rama del repositorio donde se realizará la operación Por defecto, 'main'.
 #' @export
-subir_o_actualizar <- function(nombre_archivo, repo_owner, repo_name, branch = "main") {
+subir_o_actualizar <- function(path_local, path_remoto,
+                               repo_owner, repo_name,
+                               branch = "main") {
   tryCatch({
-    tree <- gh("GET /repos/:owner/:repo/git/trees/:branch?recursive=1", owner = repo_owner, repo = repo_name, branch = branch)$tree
+    # Obtenemos lista de archivos del repo
+    tree <- gh("GET /repos/:owner/:repo/git/trees/:branch?recursive=1",
+               owner = repo_owner, repo = repo_name, branch = branch)$tree
     paths <- vapply(tree, function(x) x$path, character(1))
-    index <- grep(paste0("/", nombre_archivo, "$"), paths)
-    path_remoto <- if (length(index) > 0) paths[index] else NULL
-
-    if (!is.null(path_remoto)) {
-      message("Path encontrado en el repo: ", path_remoto)
+    
+    # Verificamos si el path remoto ya existe
+    index <- grep(paste0("^", path_remoto, "$"), paths)
+    path_remoto_in_repo <- if (length(index) > 0) paths[index] else NULL
+    
+    # Si existe, obtenemos el sha
+    if (!is.null(path_remoto_in_repo)) {
+      message("Path encontrado en el repo: ", path_remoto_in_repo)
       sha <- gh("GET /repos/:owner/:repo/contents/:path?ref=:branch",
-                owner = repo_owner, repo = repo_name, path = path_remoto, branch = branch)$sha
-      if (is.null(sha)) stop("No se pudo obtener el SHA remoto.")
+                owner = repo_owner,
+                repo = repo_name,
+                path = path_remoto_in_repo,
+                branch = branch)$sha
+      if (is.null(sha)) {
+        stop("No se pudo obtener el SHA remoto.")
+      }
     }
-
-    path_local <- file.path(tempdir(), nombre_archivo)
-    if (!file.exists(path_local)) stop(paste("El archivo local no existe en", tempdir()))
-
-    if (is.null(path_remoto)) {
-      subir_archivo(nombre_archivo, path_local, repo_owner, repo_name, branch)
+    
+    # Verificamos que el archivo local exista
+    if (!file.exists(path_local)) {
+      stop(sprintf("El archivo local no existe en la ruta '%s'", path_local))
+    }
+    
+    # Subimos o actualizamos según corresponda
+    if (is.null(path_remoto_in_repo)) {
+      # No existe en el repo, se sube como nuevo
+      subir_archivo(path_remoto, path_local, repo_owner, repo_name, branch)
     } else {
-      actualizar_archivo(path_remoto, path_local, sha, repo_owner, repo_name, branch)
+      # Ya existe, se actualiza
+      actualizar_archivo(path_remoto_in_repo, path_local, sha, repo_owner, repo_name, branch)
     }
-
+    
   }, error = function(e) {
     message("Error: ", conditionMessage(e))
   })
 }
 
-
-
 #' Subir o actualizar un archivo en repositorio data de argendatafundar
 #'
-#' @param nombre_archivo Nombre del archivo a subir o actualizar.
-#' @param branch Rama del repositorio donde se realizará la operación Por defecto, 'main'.
+#' @param filename Nombre del archivo a buscar en tmpdir.
+#' @param subtopico Ruta en el repositorio remoto donde se guardará el archivo.
+#' @param branch Rama del repositorio donde se realizará la operación Por defecto, 'dev'.
 #' @export
-#'
+mandar_data <- function(filename, subtopico, branch = "dev") {
 
-mandar_data <- function(nombre_archivo, branch = "dev") {
+ subtopico <- toupper(subtopico)
+  
+  repo <- gh::gh(endpoint = "/repos/argendatafundar/data/git/trees/main?recursive=false")
+  
+  tree <- repo$tree
+  
+  paths <- sapply(tree, function(x) {x$path})
+  
+  lista_subtopicos <- grep("^[A-Z]{6}$", paths, value = T)
+  
+  assertthat::assert_that(length(subtopico) == 1)
+  assertthat::assert_that(is.character(subtopico))
+  assertthat::assert_that(nchar(subtopico) == 6)
+  assertthat::assert_that(subtopico %in% lista_subtopicos)
 
-  subir_o_actualizar(nombre_archivo,
-                     repo_owner = "argendatafundar",
-                     repo_name = "data", branch = branch)
+  path_local <- glue::glue("{tempdir()}/{filename}")
+  path_remoto <- glue::glue("{subtopico}/{filename}")
 
+  subir_o_actualizar(
+    path_local  = path_local,
+    path_remoto = path_remoto,
+    repo_owner  = "argendatafundar",
+    repo_name   = "data",
+    branch      = branch
+  )
 }
 

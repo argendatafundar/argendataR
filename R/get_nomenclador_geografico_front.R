03025846e0f696dacac25fdae350fbcbf5fc2e9c
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-02-27 09:35:24 -0300

    Cambios en geonomeclador
    vuelvo a la función vieja de get_nomenclador_geografico. Genero función nueva get_nomenclador_geografico_front, utilizando el codigo que estaba en get_nomenclador_geografico (en la ultima version). En utils y en get_nomenclador_geografico_front cambio URL_GEONOMENCLADOR => URL_GEONOMENCLADOR_FRONT para mayor claridad

---
+++ R/get_nomenclador_geografico_front.R
@@ -0,0 +1,34 @@
+#' Devuelve el nomenclador geografico para argendata
+#'
+#' @return tibble con la sheet de consolidado_fundar del drive de Argendata
+#' @export
+#'
+
+get_nomenclador_geografico <- function() {
+  # Definir el nombre del archivo de caché
+  cache_file <- file.path(tempdir(), "nomenclador_geografico_cache_argdt.rds")
+  
+  # Verificar si el archivo de caché ya existe
+  if (file.exists(cache_file)) {
+    # Leer el nomenclador desde el caché
+    nomenclador <- readRDS(cache_file)
+  } else {
+    # Obtener la URL del nomenclador desde utils.R
+    url_nomenclador <- URL_GEONOMENCLADOR_FRONT()
+    
+    # Descargar y leer el archivo Excel directamente desde la URL
+    nomenclador <- tryCatch(
+      {
+        jsonlite::fromJSON(url_nomenclador, flatten = TRUE)
+      },
+      error = function(e) {
+        stop("Error al descargar o leer el nomenclador desde la URL: ", e$message)
+      }
+    )
+    
+    # Guardar el nomenclador en el archivo de caché
+    saveRDS(nomenclador, cache_file)
+  }
+  
+  return(nomenclador)
+}

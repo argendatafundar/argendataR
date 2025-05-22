5ff14dddecc7b323c642c0f77df234d8f0c26df2
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-05-22 11:34:39 -0300

    Parámetro 'version' en subtopicos_dir
    Se agrega parámetro version en subtopicos_dir para poder optar por el sistema de archivos nuevo montado sobre shared drive (version == 'v2'), el comportamiento default es con el sistema de archivos anterior.

--- R/subtopicos_dir.R
+++ R/subtopicos_dir.R
@@ -3,11 +3,25 @@
 #' @keywords internal
 #' @export
 
-subtopicos_dir <- function() {
+subtopicos_dir <- function(version=NULL) {
+  
+  ersion = NULL
+  search_string = 'subtopicos_dir'
+  temp_out <- tempfile(pattern = "subtopicos_dir_argdt", fileext = ".rds")
+  folder_name <- "SUBTOPICOS"
+  
+  if (version == 'v2'){
+    
+    version = 'v2'
+    search_string = 'subtopicos_dir.*v2'
+    temp_out <- tempfile(pattern = "subtopicos_dir_argdt_v2", fileext = ".rds")
+    folder_name <- "TOPICOS"
+  
+    }
+  
+  argendata_root_dir <- argendata_root_dir(version)
 
-  argendata_root_dir <- argendata_root_dir()
-
-  filetemp <- list.files(tempdir(), full.names = T)[grepl("subtopicos_dir", list.files(tempdir()))]
+  filetemp <- list.files(tempdir(), full.names = T)[grepl(search_string, list.files(tempdir()))]
 
   if (length(filetemp) == 1) {
 
@@ -16,10 +30,10 @@ subtopicos_dir <- function() {
   } else {
 
     subtopicos_dir <- list()
-    subtopicos_dir$id <- argendata_root_dir$id[argendata_root_dir$name == "SUBTOPICOS"]
+    subtopicos_dir$id <- argendata_root_dir$id[argendata_root_dir$name == folder_name]
     subtopicos_dir$tree <- googledrive::drive_ls(googledrive::as_id(subtopicos_dir$id))
 
-    readr::write_rds(subtopicos_dir, tempfile(pattern = "subtopicos_dir_argdt", fileext = ".rds"))
+    readr::write_rds(subtopicos_dir, temp_out)
 
     subtopicos_dir
 
